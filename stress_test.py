990e9a681d7280caec5d22c548c75ff3ea31f0c1
barbecuei <boat.9426@gmail.com>
2026-08-31 13:50:15 +0000

    Completed Lab 1: System Profiling

--- stress_test.py
+++ stress_test.py
@@ -1,8 +1,4 @@
-# ==========================================
-# OS-Lab 01: CPU Stress Test
-# Student ID: [Enter your ID here]
-# ==========================================
-import time
-
-# TODO: Write the intensive computation loop here
-# Follow the instructions in the Lab manual.
+import math
+print("Starting CPU Stress Test... Press Ctrl+C to stop.")
+while True:
+    math.factorial(50000) # Intensive calculation
